483e626ff71a34a2600614197eb63b20fee540b2
John Sultan <jenya.sultan@yandex.ru>
2024-02-05 23:03:35 +0300

    добавлены карточки товара

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log(`hello`);
+"use strict";
+
+const menuButton = document.querySelector(".menu__button-container");
+const burger = document.querySelector(".menu__button");
+const blockMenu = document.querySelector(".menu");
+const switchBtn = document.querySelectorAll(".requirements__switch-btn");
+const requirementsPreference = document.querySelectorAll(
+  ".requirements_preference"
+);
+menuButton.addEventListener("click", function () {
+  burger.classList.toggle("is-active");
+  blockMenu.classList.toggle("menu__hiden");
+});
+
+switchBtn.forEach((item) => {
+  item.addEventListener("click", function () {
+    item.classList.toggle("switch-on");
+    requirementsPreference.forEach((item) => {
+      item.classList.toggle("requirements-isActive");
+    });
+  });
+});
