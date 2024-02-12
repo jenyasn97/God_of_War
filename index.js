ffadee070f3e076ad132d6d4b2a11d2e8e571702
John Sultan <jenya.sultan@yandex.ru>
2024-02-12 17:56:41 +0300

    add grabCursor: true

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ const swiper = new Swiper(".swiper", {
   slidesPerView: 3,
   spaceBetween: 30,
   loop: true,
+  grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
